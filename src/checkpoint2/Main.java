d6b5396c0d8b77663e2f6325b2b561badf675636
Johan Mattsson <mattsson.cj@gmail.com>
2022-08-22 15:33:51 +0200

    En ny print

--- src/checkpoint2/Main.java
+++ src/checkpoint2/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("hej värld!");
+        System.out.println("Yttligare en ny");
     }
 }
